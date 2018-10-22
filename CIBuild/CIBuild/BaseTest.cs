5addc9043ee747cdcb33c385f7b6cce8f4e3a7c5
Anton Mityevskiy <anton.mityevskiy@gmail.com>
2018-10-22 21:50:57 +0300

    Added BaseTest

---
+++ CIBuild/CIBuild/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIBuild
+{
+    public class BaseTest
+    {
+        private IWebDriver _driver;
+
+        [Test]
+        public void FirstTest()
+        {
+            _driver = new ChromeDriver();
+
+            _driver.Navigate().GoToUrl("https:\\www.google.com");
+
+            Assert.True(true);
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+    }
+}
